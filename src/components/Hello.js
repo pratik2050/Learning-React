5a294607fe82e69b2dc202d9a3b4a882cbd4ef98
Pratik Das <pratik14901@gmail.com>
2022-03-16 00:44:33 +0530

    understanding JSX 1

---
+++ src/components/Hello.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hello = () => {
+    return (
+        <div>
+            <h1>
+                Hello Moon Without JSX
+            </h1>
+        </div>
+    )
+}
+
+export default Hello;
---
+++ src/components/HelloJSX.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const JSXHello = () => {
+    return React.createElement('div', null, React.createElement('h1', null, 'Hello Moon JSX'));
+}
+
+export default JSXHello;
