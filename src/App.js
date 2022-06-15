e68a7aac2dbc736167763515d969f435c59e2b0b
Pratik Das <pratik14901@gmail.com>
2022-06-15 20:59:37 +0530

    small update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Hello from './components/Hello';
 import JSXHello from './components/HelloJSX';
-import ProjectsSection from './components/ProjectSection';
+
 
 function App() {
   return (
@@ -13,7 +12,6 @@ function App() {
       <Welcome/>
       <Hello/>
       <JSXHello/>
-      <ProjectsSection></ProjectsSection>
     </div>
   );
 }
